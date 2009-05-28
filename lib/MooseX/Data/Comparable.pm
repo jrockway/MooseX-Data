7985a66714b22909646a5b8b8bf4a225a887c4d0
Jonathan Rockway <jon@jrock.us>
2009-05-28 01:38:55 -0700

    prototype

---
+++ lib/MooseX/Data/Comparable.pm
@@ -0,0 +1,17 @@
+use MooseX::Declare;
+
+role MooseX::Data::Comparable {
+    requires 'compare_to';
+
+    method is_equal_to($other) {
+        return $self->compare_to($other) == 0;
+    }
+
+    method is_less_than($other){
+        return $self->compare_to($other) == -1;
+    }
+
+    method is_greater_than($other){
+        return $self->compare_to($other) == 1;
+    }
+}
