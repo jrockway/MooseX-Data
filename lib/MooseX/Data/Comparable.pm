5f28dfff685df00af1c07cbbab515abd2fe9000c
Kent Fredric <kentfredric@gmail.com>
2009-05-28 22:34:28 +1200

    Added Ingore
    Put in ; notation to trick perltidy into indenting properly. This
    is annoying, but can't be helped at preset.
    
    Added is_lesser_or_equal_to and is_greater_or_equal_to with inverse
    rationale.  ( x >= y ) == (!( x < y ))

--- lib/MooseX/Data/Comparable.pm
+++ lib/MooseX/Data/Comparable.pm
@@ -5,13 +5,21 @@ role MooseX::Data::Comparable {
 
     method is_equal_to($other) {
         return $self->compare_to($other) == 0;
-    }
+    };
 
-    method is_less_than($other){
+    method is_less_than($other) {
         return $self->compare_to($other) < 0;
-    }
+    };
 
-    method is_greater_than($other){
+    method is_greater_than($other) {
         return $self->compare_to($other) > 0;
-    }
+    };
+
+    method is_lesser_or_equal_to($other) {
+        return not $self->is_greater_than($other);
+    };
+
+    method is_greater_or_equal_to($other) {
+        return not $self->is_less_than($other);
+    };
 }
